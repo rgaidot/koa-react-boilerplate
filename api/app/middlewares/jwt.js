10203f002d090df34f3bc6ea1d13f38e74e9800c
Régis Gaidot <rgaidot@gmail.com>
2017-02-17 12:32:16 +0100

    add jwt and react-helmet

---
+++ api/app/middlewares/jwt.js
@@ -0,0 +1,7 @@
+import config from 'config';
+import jwt from 'koa-jwt';
+
+module.exports = jwt({
+    secret: config.jwt.secret,
+    passthrough: config.jwt.passthrough,
+});
--- frontend/src/containers/AppContainer.js
+++ frontend/src/containers/AppContainer.js
@@ -1,9 +1,11 @@
 import React, { PropTypes } from 'react';
+import Helmet from 'react-helmet';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const AppContainer = ({ children, title = 'Koa Restfull Frontend' }) => (
     <div className="AppContainer">
+        <Helmet title={title} />
         <Header title={title} />
         <div className="main-wrapper">{children}</div>
         <Footer />
