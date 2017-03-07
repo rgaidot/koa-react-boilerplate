efdf65d24a2834526bbeab26bd818be353440c66
Régis Gaidot <rgaidot@gmail.com>
2017-03-07 21:23:27 +0100

    add cluster

--- api/app/index.js
+++ api/app/index.js
@@ -4,6 +4,9 @@ import Koa from 'koa';
 import koaMount from 'koa-mount';
 import helmet from 'koa-helmet';
 
+import cluster from 'cluster';
+import os from 'os';
+
 import bodyParser from 'koa-bodyparser';
 import cors from 'kcors';
 import chalk from 'chalk';
@@ -27,8 +30,19 @@ const app = new Koa()
 
 const main = async () => {
     const dbService = await db;
-    await app.listen(config.port, () =>
-        winston.info(chalk.black.bgGreen.bold(`${config.appName} - ${config.version}`)));
+    if (cluster.isMaster && (process.env.NODE_ENV === 'production')) {
+        os.cpus().forEach(() => cluster.fork());
+
+        cluster.on('online', worker => winston.info(`Worker ${worker.process.pid} online.`));
+        cluster.on('message', message => winston.info(message));
+        cluster.on('exit', (worker, signal) => {
+            winston.info(`Worker ${worker.process.pid} died (signal: ${signal}). Restarting...`);
+            cluster.fork();
+        });
+    } else {
+        await app.listen(config.port, () =>
+            winston.info(chalk.black.bgGreen.bold(`${config.appName} - ${config.version}`)));
+    }
 };
 
 main().catch(error => winston.error(error));
