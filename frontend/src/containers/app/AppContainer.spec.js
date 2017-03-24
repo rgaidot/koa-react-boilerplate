ab2260192c7d12bfd35f715121a80df2ed812e12
Régis Gaidot <rgaidot@gmail.com>
2017-03-24 14:50:31 +0100

    add test on AppContainer

---
+++ frontend/src/containers/app/AppContainer.spec.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import configureStore from 'redux-mock-store'
+import createFragment from 'react-addons-create-fragment'
+import expect from 'expect'
+import { shallow } from 'enzyme'
+import AppContainer from './AppContainer'
+
+const middlewares = []
+const mockStore = configureStore(middlewares)
+
+describe('<AppContainer />', () => {
+    it('render component', () => {
+        const initialState = {}
+        const store = mockStore(initialState)
+
+        const wrapper = shallow(
+            <AppContainer store={store} children={createFragment()} />,
+        )
+
+        expect(wrapper.find('AppContainer').length).toBe(1)
+    })
+})
