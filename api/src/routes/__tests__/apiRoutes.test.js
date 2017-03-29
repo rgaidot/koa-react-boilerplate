3fc322f2d0894f010feb2f5dff3c8455258c9b89
Régis Gaidot <rgaidot@gmail.com>
2017-03-29 12:51:58 +0200

    fix tests

--- api/src/routes/__tests__/apiRoutes.test.js
+++ api/src/routes/__tests__/apiRoutes.test.js
@@ -4,25 +4,22 @@ import config from 'config'
 import server from '../../server'
 
 describe('apiRoutes tests', () => {
-    it('/', (done) => {
-        request(server.listen())
+    it('GET /', async () => {
+        await request(server.listen())
           .get('/')
-          .expect('Content-Type', /json/)
           .expect(404, {
               error: 404,
               message: 'Not found',
-          }, done)
+          })
     })
 
-    it('/v1', (done) => {
-        request(server.listen())
+    it('GET /v1', async () => {
+        await request(server.listen())
           .get('/v1')
-          .expect('Content-Type', /json/)
           .expect(200, {
               appName: config.appName,
               environment: 'development',
-              url: config.prefix,
               version: config.version,
-          }, done)
+          })
     })
 })
